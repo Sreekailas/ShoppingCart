f70f956699472f182eeb1c5584a243990c8de2dd
Sree kailas <sreekailas585@gmail.com>
2022-08-07 01:13:46 +0530

    user login setup and session creating sucessfull

--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,56 @@
-const { application } = require('express');
+
 var express = require('express');
 var router = express.Router();
 var productHelpers = require('../helpers/product-helpers')
-var MongoClient = require('mongodb').MongoClient;
+const userHelpers=require('../helpers/user-helpers')
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
+  let user=req.session.user
+  console.log(user);
   productHelpers.getAllProducts().then((products) => {
-    console.log(products);
-    res.render("user/view-products", { admin: true, products })
+
+    res.render("user/view-products", {products,user })
   })
 
 });
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+  }
+)
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response);
+      req.session.loggedIn=true
+    req.session.user=response
+    res.redirect('/')
 
-router.post('/submit', (req, res) =>
-  MongoClient.connect("mongodb://localhost:27017", (err, client) => { if (err) console.log("error"); else console.log("connected"); }))
+  })
+})
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+
+    }else{
+     
+      res.redirect('/login')
+    }
+  })
+
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
+})
+
+
 
 
 module.exports = router;
